cd69a4878e0cc98938172443ef3bbfa21086cdd1
Chan Siaw Wei <siawwei98@gmail.com>
2021-06-23 00:13:30 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+var cors = require("cors");
+
+var app = express();
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
