687656461424187da59276bd4f623abe5148c24a
Chan Siaw Wei <siawwei98@gmail.com>
2021-06-23 08:45:26 +0800

    modify server render

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+const path = require("path");
 
 const express = require("express");
 
@@ -8,10 +9,16 @@ var cors = require("cors");
 
 var app = express();
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
